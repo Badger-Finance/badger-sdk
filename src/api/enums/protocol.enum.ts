f953b1d0320fd5e7602387107fecefb06ff7ec07
jintao <hellojintao@protonmail.com>
2021-11-06 16:09:47 -0400

    chore: add protocol requirement

---
+++ src/api/enums/protocol.enum.ts
@@ -0,0 +1,12 @@
+export enum Protocol {
+  Badger = 'Badger',
+  Convex = 'Convex',
+  Curve = 'Curve',
+  mStable = 'mStable',
+  Pancakeswap = 'Pancakeswap',
+  Quickswap = 'Quickswap',
+  Sushiswap = 'Sushiswap',
+  Swapr = 'Swapr',
+  Uniswap = 'Uniswap',
+  Yearn = 'Yearn',
+}
