bca34c27c534711bc731e0a2597ad03e93d0890f
jintao <hellojintao@protonmail.com>
2022-01-18 09:47:51 -0500

    chore: add missing local network support

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,6 +2,7 @@ import { ARB_CONFIG } from './network/arbitrum.config';
 import { AVAX_CONFIG } from './network/avalanche.config';
 import { BSC_CONFIG } from './network/binance.config';
 import { ETH_CONFIG } from './network/ethereum.config';
+import { LOCAL_CONFIG } from './network/local.config';
 import { MATIC_CONFIG } from './network/polygon.config';
 
 export const ONE_MIN_MS = 60 * 1000;
@@ -11,10 +12,11 @@ export const ONE_YEAR_MS = 365 * ONE_DAY_MS;
 
 export const SUPPORTED_NETWORKS = [
   ETH_CONFIG,
+  LOCAL_CONFIG,
   BSC_CONFIG,
   MATIC_CONFIG,
   ARB_CONFIG,
   AVAX_CONFIG,
 ];
 
-export const VERSION = '1.0.62';
+export const VERSION = '1.0.63';
