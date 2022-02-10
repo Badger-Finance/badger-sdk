0f8e09ec08f86a4e827e1c1d53fee48109502f54
jintao <hellojintao@protonmail.com>
2022-02-10 15:36:09 -0500

    chore: update load vault options

--- src/vaults/interfaces/index.ts
+++ src/vaults/interfaces/index.ts
@@ -1,3 +1,4 @@
+export { VaultOptions } from './vault-options.interface';
 export { VaultPerformance } from './vault-performance.interface';
 export { VaultRegistration } from './vault-registration.interface';
 export { RegistryVault } from '../../registry/interfaces/registry-vault.interface';
---
+++ src/vaults/interfaces/vault-options.interface.ts
@@ -0,0 +1,6 @@
+export interface VaultOptions {
+  update?: boolean;
+  requireRegistry?: boolean;
+  version?: string;
+  status?: number;
+}
