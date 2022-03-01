7317fa8adb4c81730a080257c44538bb1acf39cf
hunson <functionsilence@gmail.com>
2022-03-02 00:07:18 +0200

    fiat: merge, and tests fixes

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,26 +1,30 @@
-import { ethers } from 'ethers';
 import BadgerSDK, { BadgerAPI, BadgerGraph } from '.';
+import { Network } from './config';
 
-const ethAlchemyNodeUrl = 'https://eth-mainnet.alchemyapi.io/v2/-8IubgVIbg-UaXks6VLvgsuYujAravb4'
+const ethAlchemyNodeUrl = 'https://eth-mainnet.alchemyapi.io/v2/-8IubgVIbg-UaXks6VLvgsuYujAravb4';
 
-describe('BadgerSDK', async () => {
+describe('BadgerSDK',() => {
   let sdk: BadgerSDK;
+  let isSdkReady: boolean | void;
 
   beforeAll(async () => {
-    sdk = new BadgerSDK(
-      1,
-      new ethers.providers.JsonRpcBatchProvider(ethAlchemyNodeUrl),
-    );
+    const initializeFunc = jest.spyOn(BadgerSDK.prototype as any, 'initialize');
+    initializeFunc.mockImplementation(() => (new Promise((res) => res(true))));
+
+    sdk = new BadgerSDK({
+      network: Network.Ethereum,
+      provider: ethAlchemyNodeUrl
+    });
+
+    isSdkReady = Boolean(await sdk.ready());
   });
 
   // test('Services are booting properly', async () => {});
   test('Services are booting properly', async () => {
-    const isReady = await sdk.ready();
-
-    expect(isReady).toBeTruthy();
+    expect(isSdkReady).toBeTruthy();
   });
   test('Api is a part of SDK, and a standalone app', async () => {
-    const badgerApi = new BadgerAPI();
+    const badgerApi = new BadgerAPI({ network: Network.Ethereum });
 
     expect(sdk.api).toBeInstanceOf(BadgerAPI);
     expect(badgerApi).toBeInstanceOf(BadgerAPI);
@@ -29,7 +33,7 @@ describe('BadgerSDK', async () => {
   test('Graph is a part of SDK, and a standalone app', async () => {
     const badgerGraph = new BadgerGraph();
 
-    expect(sdk.graph).toBeInstanceOf(badgerGraph);
+    expect(sdk.graph).toBeInstanceOf(BadgerGraph);
     expect(badgerGraph).toBeInstanceOf(BadgerGraph);
   });
 });
