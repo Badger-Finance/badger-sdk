01f35e17ac8428ea1f3e960338289942ea9d555d
jintao <hellojintao@protonmail.com>
2022-03-14 18:00:42 -0400

    fix: update chart vault snapshot interface

--- src/api/interfaces/vault-snapshot.interface.ts
+++ src/api/interfaces/vault-snapshot.interface.ts
@@ -1,10 +1,15 @@
+import { VaultStrategy } from ".";
+
 export interface VaultSnapshot {
   address: string;
   block: number;
   timestamp: number;
+  available: number;
   balance: number;
-  supply: number;
+  totalSupply: number;
   pricePerFullShare: number;
   value: number;
+  strategy: VaultStrategy;
+  boostWeight: number;
   apr: number;
 }
