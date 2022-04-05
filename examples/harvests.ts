126fc34b0c4260d6d949940926a0d07b62cdd0c4
jintao <hellojintao@protonmail.com>
2022-04-05 17:53:21 -0400

    fix: add error logging for request failures + remove load schedule query param

--- examples/harvests.ts
+++ examples/harvests.ts
@@ -1,4 +1,4 @@
-import BadgerSDK, { Network, VaultVersion } from '@badger-dao/sdk';
+import BadgerSDK, { Network } from '@badger-dao/sdk';
 
 /**
  * Example for how to load all performance related events to a Badger Vault.
@@ -7,38 +7,22 @@ import BadgerSDK, { Network, VaultVersion } from '@badger-dao/sdk';
 async function getVaultPerformance() {
   // construct the sdk for whatever network you want to interact with
   const sdk = new BadgerSDK({
-    network: Network.Ethereum,
+    network: Network.Fantom,
     provider:
-      'https://eth-archival.gateway.pokt.network/v1/lb/<APP_ID>',
+      'https://rpc.ftm.tools',
   });
   await sdk.ready();
 
-  // load all badger productionized vaults - these appear on the app
-  const productionVaults = await sdk.registry.getProductionVaults();
-  console.log(
-    `SDK loaded ${productionVaults.length} vaults on ${sdk.config.network}`,
-  );
-  const versionOneVaults = productionVaults.filter(
-    (v) => v.version === VaultVersion.v1,
-  );
+  // // load vault metadata
+  // const vault = await sdk.vaults.loadVault({ address: '0x96d4dBdc91Bef716eb407e415c9987a9fAfb8906', requireRegistry: false, version: VaultVersion.v1_5, state: VaultState.Guarded });
+  // console.log(`Loading performance data for ${vault.name}`);
 
-  // oh typescript - we have the vaults!
-  if (!versionOneVaults) {
-    throw new Error("How'd you manage to break the example?");
-  }
-
-  const randomVault =
-    versionOneVaults[Math.floor(Math.random() * versionOneVaults.length)];
-  console.log(`Picked ${randomVault.address} for Harvests example...`);
-
-  // load vault metadata
-  const vault = await sdk.vaults.loadVault({ address: randomVault.address });
-  console.log(`Loading performance data for ${vault.name}`);
-
-  const allRecentHarvests = await sdk.vaults.listHarvests({
-    address: vault.address,
-  });
-  console.log(allRecentHarvests);
+  // const allRecentHarvests = await sdk.vaults.listHarvests({
+  //   address: vault.address,
+  //   version: VaultVersion.v1_5,
+  // });
+  // console.log(allRecentHarvests);
+  console.log(await sdk.api.loadSchedule('0xbF2F3a9ba42A00CA5B18842D8eB1954120e4a2A9', true));
 }
 
 getVaultPerformance();
