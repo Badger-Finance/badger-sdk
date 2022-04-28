ae158eddb969ed30cb08daf1f6aabeec051f07c3
jintao <hellojintao@protonmail.com>
2022-04-28 12:18:35 -0400

    fix: extend citadel treasury summary

--- src/api/interfaces/citadel-treasury-summary.interface.ts
+++ src/api/interfaces/citadel-treasury-summary.interface.ts
@@ -3,8 +3,12 @@ import { TreasurySummary } from '.';
 export interface CitadelTreasurySummary extends TreasurySummary {
   fundingBps: number;
   lockingBps: number;
+  marketCap: number;
   marketCapToTreasuryRatio: number;
+  staked: number;
+  stakedPercent: number;
   stakingBps: number;
+  supply: number;
   valueBtc: number;
   valuePaid: number;
   valuePaidBtc: number;
