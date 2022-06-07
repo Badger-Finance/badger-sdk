f92f14f103c579eeac513127b6d7266d537ed5d1
jintao <hellojintao@protonmail.com>
2022-06-07 13:33:46 -0400

    fix: update account interface extension fields

--- src/api/interfaces/account.interface.ts
+++ src/api/interfaces/account.interface.ts
@@ -3,17 +3,8 @@ import { VaultData } from './vault-data.interface';
 
 export interface Account extends UserBoostData {
   address: string;
-  boost: number;
-  boostRank: number;
-  bveCvxBalance: number;
   claimableBalances: Record<string, string>;
   data: Record<string, VaultData>;
-  diggBalance: number;
   earnedValue: number;
-  multipliers: Record<string, number>;
-  nativeBalance: number;
-  nftBalance: number;
-  nonNativeBalance: number;
-  stakeRatio: number;
   value: number;
 }
