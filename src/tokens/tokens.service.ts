4503b08be33835fb56580510f16c045eba3b3474
jintao <hellojintao@protonmail.com>
2022-06-08 15:13:43 -0400

    chore: add allowance cache + upgrade revoke options

--- src/tokens/tokens.service.ts
+++ src/tokens/tokens.service.ts
@@ -3,25 +3,53 @@ import { TransactionStatus } from '../config';
 import { Erc20, Erc20__factory } from '../contracts';
 import { Service } from '../service';
 import { IncreaseAllowanceOptions } from './interfaces/increase-allowance-options.interface';
+import { RevokeAllowanceOptions } from './interfaces/revoke-allowance-options.interface';
 import { Token } from './interfaces/token.interface';
 
 export class TokensService extends Service {
   private tokens: Record<string, Token> = {};
+  private allowances: Record<string, BigNumber> = {};
 
-  async revoke(token: string, spender: string): Promise<TransactionStatus> {
+  async revoke({
+    token,
+    spender,
+    overrides,
+    onError,
+    onRejection,
+    onApprovePrompt,
+    onApproveSigned,
+    onApproveSuccess,
+  }: RevokeAllowanceOptions): Promise<TransactionStatus> {
     let result = TransactionStatus.UserConfirmation;
     try {
       const tokenContract = Erc20__factory.connect(token, this.sdk.provider);
-      const tx = await tokenContract.approve(spender, 0);
+      if (onApprovePrompt) {
+        onApprovePrompt();
+      }
+      const tx = await tokenContract.approve(spender, 0, overrides);
       result = TransactionStatus.Pending;
+      if (onApproveSigned) {
+        onApproveSigned();
+      }
       await tx.wait();
       result = TransactionStatus.Success;
+      if (onApproveSuccess) {
+        onApproveSuccess();
+      }
+      this.allowances[ethers.utils.getAddress(token)] = ethers.constants.Zero;
     } catch (err) {
       if (result !== TransactionStatus.UserConfirmation) {
+        result = TransactionStatus.Failure;
         this.error(err);
-        return TransactionStatus.Failure;
+        if (onError) {
+          onError(err);
+        }
+      } else {
+        result = TransactionStatus.Canceled;
+        if (onRejection) {
+          onRejection();
+        }
       }
-      result = TransactionStatus.Canceled;
     }
     return result;
   }
@@ -32,8 +60,16 @@ export class TokensService extends Service {
     if (!this.address) {
       return TransactionStatus.Failure;
     }
-    const token = Erc20__factory.connect(options.token, this.provider);
-    const allowance = await token.allowance(this.address, options.token);
+
+    let allowance: BigNumber;
+    const cachedAllowance =
+      this.allowances[ethers.utils.getAddress(options.token)];
+    if (cachedAllowance) {
+      allowance = cachedAllowance;
+    } else {
+      allowance = await this.loadAllowance(this.address, options.token);
+    }
+
     if (options.amount.gt(allowance)) {
       return this.increaseAllowance(options);
     }
@@ -53,11 +89,15 @@ export class TokensService extends Service {
   }: IncreaseAllowanceOptions): Promise<TransactionStatus> {
     let result = TransactionStatus.UserConfirmation;
     try {
-      const tokenContract = Erc20__factory.connect(token, this.sdk.provider);
+      const tokenContract = Erc20__factory.connect(token, this.provider);
       if (onApprovePrompt) {
         onApprovePrompt();
       }
-      const tx = await tokenContract.increaseAllowance(spender, amount, overrides);
+      const tx = await tokenContract.increaseAllowance(
+        spender,
+        amount,
+        overrides,
+      );
       result = TransactionStatus.Pending;
       if (onApproveSigned) {
         onApproveSigned();
@@ -67,18 +107,20 @@ export class TokensService extends Service {
       if (onApproveSuccess) {
         onApproveSuccess();
       }
+      this.allowances[ethers.utils.getAddress(token)] = amount;
     } catch (err) {
       if (result !== TransactionStatus.UserConfirmation) {
+        result = TransactionStatus.Failure;
         this.error(err);
         if (onError) {
           onError(err);
         }
-        return TransactionStatus.Failure;
+      } else {
+        result = TransactionStatus.Canceled;
+        if (onRejection) {
+          onRejection();
+        }
       }
-      if (onRejection) {
-        onRejection();
-      }
-      result = TransactionStatus.Canceled;
     }
     return result;
   }
@@ -179,7 +221,9 @@ export class TokensService extends Service {
     try {
       const checksumAddress = ethers.utils.getAddress(token);
       const contract = Erc20__factory.connect(checksumAddress, this.provider);
-      return contract.allowance(targetAddress, spender);
+      const allowance = await contract.allowance(targetAddress, spender);
+      this.allowances[checksumAddress] = allowance;
+      return allowance;
     } catch (err) {
       this.error(err);
       return BigNumber.from(0);
