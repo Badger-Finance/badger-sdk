f0a3e60029849d3d7fae59c914fc062c15740d9d
jintao <hellojintao@protonmail.com>
2022-06-08 12:24:46 -0400

    chore: add initial testing harness functions

---
+++ src/tests/tests.utils.ts
@@ -0,0 +1,63 @@
+import { mock } from 'jest-mock-extended';
+import { BadgerSDK } from '../sdk';
+import { JsonRpcProvider, JsonRpcSigner } from '@ethersproject/providers';
+import { Network } from '../config';
+import { RewardsService } from '../rewards';
+import { RegistryService } from '../registry';
+import { TEST_ADDR } from './tests.constants';
+import {
+  Controller,
+  Vault,
+  VaultV15,
+  Strategy,
+  StrategyV15,
+  Controller__factory,
+  Vault__factory,
+  VaultV15__factory,
+  Strategy__factory,
+  StrategyV15__factory,
+} from '../contracts';
+import { MockVaultSystem } from './interfaces/mock-vault-system.interface';
+
+export function mockSDK(): BadgerSDK {
+  const mockSigner = mock<JsonRpcSigner>();
+  mockSigner.getAddress.calledWith().mockImplementation(async () => TEST_ADDR);
+  const mockProvider = mock<JsonRpcProvider>();
+  mockProvider.getSigner.calledWith().mockImplementation(() => mockSigner);
+
+  const sdk = new BadgerSDK({
+    network: Network.Fantom,
+    provider: mockProvider,
+  });
+
+  jest.spyOn(RegistryService.prototype, 'ready').mockImplementation();
+  jest.spyOn(RewardsService.prototype, 'ready').mockImplementation();
+
+  return sdk;
+}
+
+export function mockVaults(): MockVaultSystem {
+  const controller = mock<Controller>();
+  const vault = mock<Vault>();
+  const vaultV15 = mock<VaultV15>();
+  const strategy = mock<Strategy>();
+  const strategyV15 = mock<StrategyV15>();
+
+  jest
+    .spyOn(Controller__factory, 'connect')
+    .mockImplementation(() => controller);
+  jest.spyOn(Vault__factory, 'connect').mockImplementation(() => vault);
+  jest.spyOn(VaultV15__factory, 'connect').mockImplementation(() => vaultV15);
+  jest.spyOn(Strategy__factory, 'connect').mockImplementation(() => strategy);
+  jest
+    .spyOn(StrategyV15__factory, 'connect')
+    .mockImplementation(() => strategyV15);
+
+  return {
+    controller,
+    vault,
+    vaultV15,
+    strategy,
+    strategyV15,
+  };
+}
